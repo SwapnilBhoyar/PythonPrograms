a849783305a1877c903855a393fa99cdedf7f9f8
SwapnilBhoyar <swapnilbhoyar95@gmail.com>
2021-07-04 00:57:27 +0530

    [Swapnil] Add: Implemented reverse array

---
+++ Array/ReverseOrder.py
@@ -0,0 +1,39 @@
+"""
+@Author: Swapnil Bhoyar
+@Date: 2021-07-04 12:20:00
+@Last Modified by: Rikesh Chhetri
+@Last Modified time: 2021-07-03 12:20:00
+@Title : this program reverse the elements of array.
+"""
+from array import *
+import logging
+
+class ReverseOrderLog:
+    logging.basicConfig(filename="ReverseOrderLog.log",
+                    format='%(asctime)s %(message)s',
+                    filemode='w')
+  
+    #Creating an object
+    logger=logging.getLogger()
+    
+    #Setting the threshold of logger to ERROR
+    logger.setLevel(logging.ERROR)
+    
+class ReverseOrder:
+    def getReverse(self):
+        """
+        Description:
+            function reverse the array
+        """
+        
+        integerArray = array('i', [10,20,30,40,50])
+        
+        res_arr=array('i',reversed(integerArray))
+        print("Resultant Reversed Array:",res_arr)
+
+if __name__ == "__main__":
+    reverseOrder = ReverseOrder()
+    try:
+        reverseOrder.getReverse()
+    except Exception as e:
+        ReverseOrderLog.logger.error(e)
