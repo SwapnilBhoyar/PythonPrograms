b3a31d93404459b48ac27cf87996c9430ae83d2b
SwapnilBhoyar <swapnilbhoyar95@gmail.com>
2021-07-04 16:20:09 +0530

    [Swapnil] Add: implemented iterate dict

---
+++ DataStructures/Dictionary/IterateDict.py
@@ -0,0 +1,38 @@
+"""
+@Author: Swapnil Bhoyar
+@Date: 2021-07-04 16:18:00
+@Last Modified by: Rikesh Chhetri
+@Last Modified time: 2021-07-04 16:18:00
+@Title : this program iterate dictionary.
+"""
+import logging
+
+class IterateLog:
+    logging.basicConfig(filename="IterateLog.log",
+                    format='%(asctime)s %(message)s',
+                    filemode='w')
+  
+    #Creating an object
+    logger=logging.getLogger()
+    
+    #Setting the threshold of logger to ERROR
+    logger.setLevel(logging.ERROR)
+
+class IterateDict:
+    """
+    Description:
+        this function iterate dictionary
+    """
+    def iterate(self):
+        dctionary = {'Red': 1, 'Green': 2, 'Blue': 3}
+
+        for colorKey, value in dctionary.items():
+            print(colorKey, ':', dctionary[colorKey]) 
+
+if __name__=="__main__":
+    iterateDict = IterateDict()
+    try:
+        iterateDict.iterate()
+    except Exception as e:
+        IterateLog.logger(e)
+
