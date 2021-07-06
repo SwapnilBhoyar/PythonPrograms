e3a067bd7354bc05ff40220aff952a9081232fd0
SwapnilBhoyar <swapnilbhoyar95@gmail.com>
2021-07-06 15:27:47 +0530

    [Swapnil] Add: create tuple

---
+++ DataStructures/Tuple/CreateTuple.py
@@ -0,0 +1,18 @@
+"""
+@Author: Swapnil Bhoyar
+@Date: 2021-07-06 15:26:00
+@Last Modified by: Swapnil Bhoyar
+@Last Modified time: 2021-07-05 15:26:00
+@Title : Write a Python program to create a tuple.
+"""
+from Log import Log
+def createTuple():
+    """
+    Description:
+        this function create tuple
+    """
+    tempTuple = ("swapnil", 23, 2.3)
+    Log.logger.info(tempTuple)
+
+createTuple()
+
