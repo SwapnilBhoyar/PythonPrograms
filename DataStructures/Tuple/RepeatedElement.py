a193c1bad8a07c1784aa85879c3f8ec50ee4538f
SwapnilBhoyar <swapnilbhoyar95@gmail.com>
2021-07-06 17:48:33 +0530

    [Swapnil] Add: duplicate element

---
+++ DataStructures/Tuple/RepeatedElement.py
@@ -0,0 +1,24 @@
+"""
+@Author: Swapnil Bhoyar
+@Date: 2021-07-06 17:47:00
+@Last Modified by: Swapnil Bhoyar
+@Last Modified time: 2021-07-05 17:47:00
+@Title : Write a Python program to find the repeated items of a tuple.
+"""
+
+from Log import Log
+
+def repeatedElement():
+    """
+    Description:
+        this function find duplicate element
+    """
+    numberSet = set([])
+    tupple = (1,3,4,32,1,1,1,31,32,12,21,2,3) 
+    #count() count number of times repeatation of a value 
+    for number in tupple:
+        if tupple.count(number) > 1:
+            numberSet.add(number)
+    Log.logger.info(numberSet)
+
+repeatedElement()
